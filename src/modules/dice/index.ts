afcae3a16da582416b76acc386c54b8b5675ebcd
yasakaa <130965944+yasakaa@users.noreply.github.com>
2025-05-24 12:24:16 +0900

    dice更新

--- src/modules/dice/index.ts
+++ src/modules/dice/index.ts
@@ -1,77 +1,138 @@
-import autobind from "autobind-decorator";
-import Module from "@/module";
-import Message from "@/message";
-import serifs from "@/serifs";
+import 藍 from '@/ai';
+import autobind from 'autobind-decorator';
+import Module from '@/module';
+import Message from '@/message';
+import serifs from '@/serifs';
+import seedrandom from 'seedrandom';
+import includes from '@/utils/includes';
 
 export default class extends Module {
-	public readonly name = "dice";
+  public readonly name = 'dice';
 
-	@autobind
-	public install() {
-		return {
-			mentionHook: this.mentionHook,
-		};
-	}
+  @autobind
+  public install() {
+    return {
+      mentionHook: this.mentionHook,
+    };
+  }
 
-	@autobind
-	private async mentionHook(msg: Message) {
-		if (msg.text == null) return false;
+  @autobind
+  private async mentionHook(msg: Message) {
+    if (msg.extractedText == null) return false;
 
-		if (
-			msg.text.endsWith("ですか？") ||
-			msg.text.endsWith("ますか？") ||
-			msg.text.endsWith("ですよね？")
-		) {
-			if (Math.random() < 0.5) {
-				if (Math.random() < 0.25) {
-					msg.reply("多分そうじゃ、部分的にそうじゃ", { visibility: "public" });
-				} else {
-					msg.reply("そうじゃ", { visibility: "public" });
-				}
-			} else {
-				if (Math.random() < 0.25) {
-					msg.reply("多分ちがうのじゃ、部分的にちがうのじゃ", {
-						visibility: "public",
-					});
-				} else {
-					msg.reply("ちがうのじゃ", { visibility: "public" });
-				}
-			}
-			return {
-				reaction: ":neofox_heart:",
-			};
-		}
+    if (
+      msg.extractedText.endsWith('ですか？') ||
+      msg.extractedText.endsWith('ますか？') ||
+      msg.extractedText.endsWith('ですよね？')
+    ) {
+      const rng = seedrandom(msg.extractedText.trim() + ':q');
+      const v = rng();
+      if (v < 0.5) {
+        if (v < 0.25) {
+          msg.reply('多分そうじゃ、部分的にそうじゃ', { visibility: 'public' });
+        } else {
+          msg.reply('そうじゃ', { visibility: 'public' });
+        }
+      } else {
+        if (v > 0.75) {
+          msg.reply('多分ちがうのじゃ、部分的にちがうのじゃ', {
+            visibility: 'public',
+          });
+        } else {
+          msg.reply('ちがうのじゃ', { visibility: 'public' });
+        }
+      }
+      return {
+        reaction: 'love',
+      };
+    }
 
-		const query = msg.text.match(/([0-9]+)[dD]([0-9]+)/);
+    if (
+      msg.replyId &&
+      includes(msg.extractedText, ['ファクト']) &&
+      includes(msg.extractedText, ['チェック'])
+    ) {
+      if (msg.replyNote.uri) {
+        const replyUser = await this.ai.api('users/show', {
+          userId: msg.replyNote.userId,
+        });
+        if (!replyUser.isFollowed && !replyUser.isFollowing) {
+          msg.reply(
+            'わらわをフォローしていないリモートユーザはファクトチェックできぬのじゃ',
+            { visibility: 'specified' },
+          );
+          return {
+            reaction: ':neofox_approve:',
+          };
+        }
+      }
 
-		if (query == null) return false;
+      const opt =
+        msg.replyNote.visibility == 'followers' ||
+        msg.replyNote.visibility == 'specified'
+          ? { visibility: 'public', references: [msg.replyId] }
+          : { visibility: 'public', renote: msg.replyId };
 
-		const times = parseInt(query[1], 10);
-		const dice = parseInt(query[2], 10);
+      if (msg.replyNote.userId == this.ai.account.id) {
+        msg.reply(
+          '\nこの投稿はもちろん、正しいのじゃ！…どうして疑うのじゃ？',
+          opt,
+        );
+        return {
+          reaction: 'love',
+        };
+      }
 
-		if (times < 1) return false;
-		if (dice < 2 || dice > Number.MAX_SAFE_INTEGER) return false;
+      const rng = seedrandom(msg.replyId + ':f');
+      const v = rng();
+      if (v < 0.5) {
+        if (v < 0.25) {
+          msg.reply('\nこの投稿は多分正しいのじゃ、部分的に正しいのじゃ', opt);
+        } else {
+          msg.reply('\nこの投稿は正しいかもしれぬ', opt);
+        }
+      } else {
+        if (v > 0.75) {
+          msg.reply('\nこの投稿は多分嘘じゃ、部分的に嘘じゃ', opt);
+        } else {
+          msg.reply('\nこの投稿は嘘かもしれぬ', opt);
+        }
+      }
+      return {
+        reaction: 'love',
+      };
+    }
 
-		if ((dice.toString().length + 1) * times > 7000) return false;
+    const query = msg.extractedText.match(/([0-9]+)[dD]([0-9]+)/);
 
-		const results: number[] = [];
+    if (query == null) return false;
 
-		for (let i = 0; i < times; i++) {
-			results.push(Math.floor(Math.random() * dice) + 1);
-		}
+    const times = parseInt(query[1], 10);
+    const dice = parseInt(query[2], 10);
 
-		msg.reply(
-			serifs.dice.done(
-				results.join(" "),
-				results.length > 1
-					? results.reduce((a, c) => a + c).toLocaleString()
-					: null
-			),
-			{ visibility: "public" }
-		);
+    if (times < 1) return false;
+    if (dice < 2 || dice > Number.MAX_SAFE_INTEGER) return false;
 
-		return {
-			reaction: ":neofox_heart:",
-		};
-	}
+    if ((dice.toString().length + 1) * times > 7000) return false;
+
+    const results: number[] = [];
+
+    for (let i = 0; i < times; i++) {
+      results.push(Math.floor(Math.random() * dice) + 1);
+    }
+
+    msg.reply(
+      serifs.dice.done(
+        results.join(' '),
+        results.length > 1
+          ? results.reduce((a, c) => a + c).toLocaleString()
+          : null,
+      ),
+      { visibility: 'public' },
+    );
+
+    return {
+      reaction: 'love',
+    };
+  }
 }
